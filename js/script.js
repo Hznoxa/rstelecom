f9845717415b13c1055998b3beb75cdeabcfc902
Hznoxa <141236451+Hznoxa@users.noreply.github.com>
2026-03-12 01:17:52 +0530

    admin page script.js

--- js/script.js
+++ js/script.js
@@ -61,3 +61,56 @@ track.style.transform = "translateX(-" + index * 100 + "%)";
 },3000);
 
 });
+
+/* ADMIN PRODUCT SYSTEM */
+
+function addProduct(){
+
+let name = document.getElementById("name").value;
+let price = document.getElementById("price").value;
+let offer = document.getElementById("offer").value;
+let image = document.getElementById("image").value;
+
+let products = JSON.parse(localStorage.getItem("phones")) || [];
+
+products.push({name,price,offer,image});
+
+localStorage.setItem("phones", JSON.stringify(products));
+
+alert("Product Added");
+
+showAdminProducts();
+
+}
+
+function showAdminProducts(){
+
+let products = JSON.parse(localStorage.getItem("phones")) || [];
+
+let container = document.getElementById("admin-products");
+
+if(!container) return;
+
+container.innerHTML="";
+
+products.forEach(phone => {
+
+container.innerHTML += `
+<div class="product-card">
+
+<img src="${phone.image}">
+
+<h3>${phone.name}</h3>
+
+<p>${phone.price}</p>
+
+<p class="offer">${phone.offer}</p>
+
+</div>
+`;
+
+});
+
+}
+
+showAdminProducts();
