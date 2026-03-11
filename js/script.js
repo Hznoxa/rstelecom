286b6ae06ab5b65a8d478b99d07ee3a5a8a4549a
Hznoxa <141236451+Hznoxa@users.noreply.github.com>
2026-03-11 23:09:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,5 +7,33 @@ drawer.style.left="-250px";
 }else{
 drawer.style.left="0px";
 }
+}
+async function loadProducts(){
+
+let response = await fetch("data/products.json");
+
+let data = await response.json();
+
+let container = document.getElementById("product-list");
+
+data.phones.forEach(phone => {
+
+container.innerHTML += `
+<div class="product-card">
+
+<img src="${phone.image}">
+
+<h3>${phone.name}</h3>
+
+<p>${phone.price}</p>
+
+<p class="offer">${phone.offer}</p>
+
+</div>
+`;
+
+});
 
 }
+
+loadProducts();
